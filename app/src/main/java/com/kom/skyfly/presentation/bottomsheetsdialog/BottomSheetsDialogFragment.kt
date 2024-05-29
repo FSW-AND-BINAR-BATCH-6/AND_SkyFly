791e6b8d7753547989d5dc0d5cbce539ef42c9e6
YudaSaputraa <yudasaputra082@gmail.com>
2024-05-29 22:31:30 +0700

    add UI for ticket detail, add bottom sheets template, Web View for payment

---
+++ app/src/main/java/com/kom/skyfly/presentation/bottomsheetsdialog/BottomSheetsDialogFragment.kt
@@ -0,0 +1,23 @@
+package com.kom.skyfly.presentation.bottomsheetsdialog
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.kom.skyfly.R
+import com.kom.skyfly.databinding.FragmentBottomSheetsDialogBinding
+
+class BottomSheetsDialogFragment : BottomSheetDialogFragment() {
+    private lateinit var binding: FragmentBottomSheetsDialogBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentBottomSheetsDialogBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+}
