e346e3718c6a26f6fd53b7c3e27cc8a4deae32fa
unknown <ihsan.widagdo@gmail.com>
2024-06-19 00:59:59 +0700

    feat: proggres fetch api fligh

--- app/src/main/java/com/kom/skyfly/presentation/home/search_result/SearchResultViewModel.kt
+++ app/src/main/java/com/kom/skyfly/presentation/home/search_result/SearchResultViewModel.kt
@@ -1,3 +1,22 @@
 package com.kom.skyfly.presentation.home.search_result
 
-class SearchResultViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.kom.skyfly.data.repository.home.flight_ticket.FlightTicketRepository
+import kotlinx.coroutines.Dispatchers
+
+class SearchResultViewModel(
+    private val flightRepository: FlightTicketRepository,
+) : ViewModel(){
+    fun getAllFlightTicket(
+        search: String?,
+        page: Int?,
+        departureAirport: String,
+        arrivalAirport: String,
+        departureDate: String,
+        arrivalDate: String
+    ) =
+        flightRepository.getAllTicket(
+            search = search, page = page, departureAirport = departureAirport, arrivalAirport = arrivalAirport, departureDate = departureDate, arrivalDate = arrivalDate
+        ).asLiveData(Dispatchers.IO)
+}
